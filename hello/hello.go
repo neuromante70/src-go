dc903721752dedd000a92eba3c24f55eef135ef1
Filippo Montecchio <neuromante.70@gmail.com>
2022-05-13 23:11:34 +0200

    2022-05-13

--- hello/hello.go
+++ hello/hello.go
@@ -2,7 +2,12 @@ package main
 
 import "fmt"
 
+func function(int) int {
+	return 0
+}
+
 func main() {
 	// fmt.Print("\033[2J") //clear the screen befor printing the output in the terminal
-	fmt.Printf("\nhello, world\n")
+	result := function(1)
+	fmt.Println("\nYour function result is:\n", result)
 }
